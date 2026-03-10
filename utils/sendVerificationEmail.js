6ab6ea568b518ca30332a611ced05a8108a4223e
ZACHARIA KIMANI <kimani.26317@students.kyu.ac.ke>
2026-03-10 14:56:24 +0300

    Update sendVerificationEmail.js

--- utils/sendVerificationEmail.js
+++ utils/sendVerificationEmail.js
@@ -1,28 +1,27 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: process.env.VERIFICATION_EMAIL,
-        pass: process.env.VERIFICATION_EMAIL_PASS,
-    },
-    connectionTimeout: 5000,
-    greetingTimeout: 3000,
-    socketTimeout: 5000,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.VERIFICATION_EMAIL,
+    pass: process.env.VERIFICATION_EMAIL_PASS
+  }
 });
 
 module.exports = async (toEmail, token) => {
-    const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${token}`;
-    const mailOptions = {
-        from: `"BusinessEmailSender" <${process.env.VERIFICATION_EMAIL}>`,
-        to: toEmail,
-        subject: 'Verify your email address',
-        html: `
-            <p>Please click the link below to verify your email address. This link expires in 15 minutes.</p>
-            <p><a href="${verificationLink}">${verificationLink}</a></p>
-            <p>If you didn't request this, please ignore this email.</p>
-            <p><small>Check your spam folder if you don't see it.</small></p>
-        `,
-    };
-    await transporter.sendMail(mailOptions);
+  const verificationLink = `${process.env.FRONTEND_URL}/verify-email?token=${token}`;
+  const mailOptions = {
+    from: `"BusinessEmailSender" <${process.env.VERIFICATION_EMAIL}>`,
+    to: toEmail,
+    subject: 'Verify your email address',
+    html: `
+      <p>Please click the link below to verify your email address. This link expires in 15 minutes.</p>
+      <p><a href="${verificationLink}">${verificationLink}</a></p>
+      <p>If you didn't request this, please ignore this email.</p>
+      <p><small>Check your spam folder if you don't see it.</small></p>
+    `,
+  };
+  await transporter.sendMail(mailOptions);
 };
