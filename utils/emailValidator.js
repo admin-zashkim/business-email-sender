cab48be9ef28df6fd94315ee7266b3b829334d1c
ZACHARIA KIMANI <kimani.26317@students.kyu.ac.ke>
2026-03-10 14:57:05 +0300

    Update emailValidator.js

--- utils/emailValidator.js
+++ utils/emailValidator.js
@@ -1,24 +1,22 @@
 const nodemailer = require('nodemailer');
 
 async function validateAppPassword(email, appPassword) {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: { user: email, pass: appPassword },
-        connectionTimeout: 5000,
-        greetingTimeout: 3000,
-        socketTimeout: 5000,
-        tls: { rejectUnauthorized: false },
-    });
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: { user: email, pass: appPassword }
+  });
 
-    try {
-        await Promise.race([
-            transporter.verify(),
-            new Promise((_, reject) => setTimeout(() => reject(new Error('timeout')), 7000)),
-        ]);
-        return true;
-    } catch {
-        return false;
-    }
+  try {
+    await Promise.race([
+      transporter.verify(),
+      new Promise((_, reject) => setTimeout(() => reject(new Error('timeout')), 7000))
+    ]);
+    return true;
+  } catch {
+    return false;
+  }
 }
 
 module.exports = { validateAppPassword };
